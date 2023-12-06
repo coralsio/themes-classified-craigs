16f52c5a770791975c362e4ca9e7877f01e859b5
moayad <moyadisleem2@gmail.com>
2023-12-06 21:21:50 +0200

    add theme files

---
+++ resources/themes/classified-craigs/tt_create.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.public')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 py-5">
+                <div class="alert alert-warning text-light">
+                    Sorry!! Create ticket Not supported for this theme by default, contact support to do customization.
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
